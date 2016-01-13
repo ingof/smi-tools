b56e690b655eb0b82243140d7bd9a8218aa500a6
ingof <ems-gateway@arcor.de>
2016-01-13 22:16:28 +0100

    Update smi-monitor.c

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -39,7 +39,7 @@ int main( int argc, char* argv[] ) {
    /* third parameter is serialWait in ms*/
    if (argc > 3)
    {
-   	serialWait=argv[3];
+   	serialWait=atoi(argv[3]);
    } else {
    	serialWait=36;
    }
