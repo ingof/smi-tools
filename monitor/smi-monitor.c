2a0b2ae2ee31b3e25156eb183f9cb3cff2738328
ingof <ems-gateway@arcor.de>
2016-01-20 09:54:12 +0100

    Update smi-monitor.c

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -158,7 +158,7 @@ for (loop=0; ; loop++)
 	if (bytesSwb>0)
 	{
 		printf("Swb ---\n");
-	    	bufferSwb=buffer;
+	    	bufferSwb[]=buffer[];
 	    	serialSwbCount=bytesSwb;
 		if (bytesSwb == -1)
 		{
@@ -198,7 +198,7 @@ for (loop=0; ; loop++)
 	if (bytesSmi>0)
 	{
 		printf("Smi ---\n");
-	    	bufferSmi=buffer;
+	    	bufferSmi[]=buffer[];
 	    	serialSmiCount=bytesSmi;
 		if (bytesSmi == -1)
 		{
