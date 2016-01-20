22d85cd2d11013027306e8a32fc5b39b9a668ede
ingof <ems-gateway@arcor.de>
2016-01-20 20:52:43 +0100

    Update smi-monitor.c

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -225,8 +225,8 @@ int getch_nb(void) {
   term.c_cc[VMIN] = 0;
   term.c_cc[VTIME] = 1;
   tcsetattr(fd, TCSANOW, &term);
-  c = getwchar();
-  printf("|%d ");
+  c = getchar();
+  printf("|%d ",c);
   tcsetattr(fd, TCSANOW, &oterm);
   return c; // gibt -1 zurück, wenn kein Zeichen gelesen wurde
 }
