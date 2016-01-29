cb0c237ed79e202bd59a7a6fae8e836453ddaaff
ingof <ems-gateway@arcor.de>
2016-01-29 18:10:26 +0100

    .

--- monitor/smi-monitor.h
+++ monitor/smi-monitor.h
@@ -1,9 +1,9 @@
 typedef unsigned char uint8_t;
 typedef unsigned int  uint16_t;
 
-void addSwbCrc(char *buffer, int size);
-void printBuffer(char *buffer, int size);
-uint16_t createSwbCRC(char *buffer, int size);
-
 int checkSwbCRC(char *dataBuffer, int bufferSize);
 int checkSmiCRC(char *dataBuffer, int bufferSize);
+
+void printBuffer(char *buffer, int size);
+void addSwbCrc(char *data, int size);
+uint16_t createSwbCRC(char *buffer, int size);
