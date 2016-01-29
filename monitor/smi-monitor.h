0f0ae78247de92d1b8c572ba59f98ec9b4fab7dd
ingof <ems-gateway@arcor.de>
2016-01-29 19:07:20 +0100

    .

--- monitor/smi-monitor.h
+++ monitor/smi-monitor.h
@@ -5,6 +5,7 @@ int checkSwbCrc(char *buffer, int size);
 int checkSmiCrc(char *buffer, int size);
 
 void addSwbCrc(char *buffer, int size);
+void createSwbAck(char *buffer, int size);
 
 void printBuffer(char *buffer, int size);
 uint16_t createSwbCRC(char *buffer, int size);
