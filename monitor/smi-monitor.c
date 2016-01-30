c588a6be24234f977404bc1659627b10e5f795b9
ingof <ems-gateway@arcor.de>
2016-01-30 17:11:20 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -282,7 +282,7 @@ int main( int argc, char* argv[] ) {
 				if (tmp2Ret==0) {
 					createSwbAck(tmp2Buf,tmp2Siz);
 					// write(fdSwb,&tmp2Buf,tmp2Siz);
-					write(fdSwb,&tmp2Buf,7);
+					// write(fdSwb,&tmp2Buf,7);
 				} else {
 					tmp2Err++;
 					if (tmp2Err<=20) {
