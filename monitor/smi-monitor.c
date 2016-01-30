6195ed35a8465b505f1379115dbe14ca6ea81077
ingof <ems-gateway@arcor.de>
2016-01-30 18:02:20 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -133,7 +133,7 @@ int main( int argc, char* argv[] ) {
 	// cfsetispeed(&options, B19200);
 	// cfsetospeed(&options, B19200);
 
-	printf("SWB: %d (%dms, div:%d)\n",(2304000/serialSwb0Speed),serialSwbWait,serialSwb0Speed);
+	printf("SWB: %d (%dms, div:%d)\n",(23040000/serialSwb0Speed),serialSwbWait,serialSwb0Speed);
 
 	/* Enable the receiver and set local mode... */
 	options.c_cflag |= (CLOCAL | CREAD);
@@ -279,28 +279,28 @@ int main( int argc, char* argv[] ) {
 				bufferSwbCount=0;
 
 				// #############################
-				if (tmp2Ret==0) {
-					createSwbAck(tmp2Buf,tmp2Siz);
-					// write(fdSwb,&tmp2Buf,tmp2Siz);
-					// write(fdSwb,&tmp2Buf,7);
-				} else {
-					tmp2Err++;
-					if (tmp2Err<=20) {
-						printf("\033[31m   *  N A C K  *\033[m");
-						// ioctl(fdSwb, TIOCGSERIAL, &ser);
-						// // ser.flags=(ser.flags&(~ASYNC_SPD_MASK));
-						// /* divisor for 25000 kBit/s (alias 38400) */
-						// ser.custom_divisor=serialSwb0Speed++;
-						// printf("  **%d**  ",serialSwb0Speed);
-						// ioctl(fdSwb, TIOCSSERIAL, &ser);
-					} else {
-						printf("\007\033[m\033[41m\033[1m   !  S T O P  !\033[40m\033[m");
-						tmp2Err=0;
-						createSwbAck(tmp2Buf,tmp2Siz);
-						// write(fdSwb,&tmp2Buf,tmp2Siz);
-						//write(fdSwb,&tmp2Buf,7);
-					}
-				}
+				// if (tmp2Ret==0) {
+				// 	createSwbAck(tmp2Buf,tmp2Siz);
+				// 	// write(fdSwb,&tmp2Buf,tmp2Siz);
+				// 	// write(fdSwb,&tmp2Buf,7);
+				// } else {
+				// 	tmp2Err++;
+				// 	if (tmp2Err<=20) {
+				// 		printf("\033[31m   *  N A C K  *\033[m");
+				// 		// ioctl(fdSwb, TIOCGSERIAL, &ser);
+				// 		// // ser.flags=(ser.flags&(~ASYNC_SPD_MASK));
+				// 		// /* divisor for 25000 kBit/s (alias 38400) */
+				// 		// ser.custom_divisor=serialSwb0Speed++;
+				// 		// printf("  **%d**  ",serialSwb0Speed);
+				// 		// ioctl(fdSwb, TIOCSSERIAL, &ser);
+				// 	} else {
+				// 		printf("\007\033[m\033[41m\033[1m   !  S T O P  !\033[40m\033[m");
+				// 		tmp2Err=0;
+				// 		createSwbAck(tmp2Buf,tmp2Siz);
+				// 		// write(fdSwb,&tmp2Buf,tmp2Siz);
+				// 		//write(fdSwb,&tmp2Buf,7);
+				// 	}
+				// }
 				fflush(stdout); // Will now print everything in the stdout buffer
 			}
 		}
