a4a2be66bb8154004fd66a626130e70f6ba728b6
ingof <ems-gateway@arcor.de>
2016-02-03 19:39:38 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -78,6 +78,7 @@ int main( int argc, char* argv[] ) {
 	}
 
 	fdSwb=openSwbPortDiv(serialSwbPort, serialSwbDivisor);
+	printf(serialSwbPort"\n");
 	if (fdSwb==-1) {
 		perror("Unable to open serial SWB-port ");
 	} else {
