b8a67de1fbff2cced45c939e6ff49f0074028e80
ingof <ems-gateway@arcor.de>
2016-02-14 19:09:13 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -148,7 +148,7 @@ int main( int argc, char* argv[] ) {
 		printf("L");
 		fflush(stdout);
 
-		setNonblocking(mySocket);
+		// setNonblocking(mySocket);
 		if ((new_socket = accept(mySocket, (struct sockaddr *) &address, &addrlen)) < 0) {
 			perror("webserver accept");
 			printf("%d ",new_socket);
