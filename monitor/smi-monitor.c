c03f57ee635c9845b7f0188f5e5033acdfea8465
ingof <ems-gateway@arcor.de>
2016-02-17 09:50:21 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -158,7 +158,9 @@ int main( int argc, char* argv[] ) {
 			}
 			memset(bufferHTTP, 0, bufsize);
 			recv(new_socket, bufferHTTP, bufsize, 0);
-			bufferHTTP[500]="\n";
+			bufferHTTP[500]=0x0d;
+			bufferHTTP[501]=0x0a;
+
 			printf("%s", bufferHTTP);
 			write(new_socket, "HTTP/1.1 200 OK\n", 16);
 			write(new_socket, "Content-length: 46\n", 19);
