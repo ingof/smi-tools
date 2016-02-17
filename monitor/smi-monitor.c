62ea6f9ce2262c4d160e3f5251734dc97d126c80
ingof <ems-gateway@arcor.de>
2016-02-17 21:14:39 +0100

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -160,16 +160,16 @@ int main( int argc, char* argv[] ) {
 			memset(bufferHTTP, 0, bufsize);
 			recv(new_socket, bufferHTTP, bufsize, 0);
 			printf("%s", bufferHTTP);
-			/* receive posted data */
-			memset(bufferHTTP, 0, bufsize);
-			recv(new_socket, bufferHTTP, bufsize, 0);
-			printf("%s\n", bufferHTTP);
-			fflush(stdout);
 			/* send response */
 			write(new_socket, "HTTP/1.1 200 OK\n", 16);
 			write(new_socket, "Content-length: 46\n", 19);
 			write(new_socket, "Content-Type: text/html\n\n", 25);
 			write(new_socket, "<html><body><H1>Hello world</H1></body></html>",46);
+			/* receive posted data */
+			memset(bufferHTTP, 0, bufsize);
+			recv(new_socket, bufferHTTP, bufsize, 0);
+			printf("%s\n", bufferHTTP);
+			fflush(stdout);
 			/* close this socket */
 			close(new_socket);
 		}
