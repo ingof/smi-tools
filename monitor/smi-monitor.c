604675d85ec303a68c63bef6297787d7005b1289
ingof <ingo.fischer@arcormail.de>
2017-08-15 10:42:29 +0200

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -47,20 +47,20 @@ int main( int argc, char* argv[] ) {
 
 	int bytesSwb=0;
 	int bufferSwbCount=0;
-	unsigned char bufferSwb[200];
+	unsigned char bufferSwb[50];
 
 	int bytesSmi=0;
 	int bufferSmiCount=0;
-	unsigned char bufferSmi[200];
+	unsigned char bufferSmi[50];
 
 	unsigned char tmpBuffer[40];
 
 	//smi transmit
-	char smiTxBuffer[200];
+	char smiTxBuffer[50];
 	char smiTxSize=0;
 
 	// temporary test use:
-	unsigned char tmp2Buf[200];
+	unsigned char tmp2Buf[50];
 	int tmp2Ret;
 	int tmp2Siz;
 	int tmp2Err;
@@ -145,59 +145,59 @@ int main( int argc, char* argv[] ) {
 	/* endless-loop */
 	for (loop=0; ;loop++) {
 		if (loop>=0x80000000) loop=0;
-		//
-		//
-		// /* web server */
-		// tmpListen=listen(mySocket, 10);
-		// if (tmpListen < 0) {
-		// 	perror("webserver listen");
-		// 	exit(1);
-		// }
-		//
-		// setNonblocking(mySocket);
-		// if ((new_socket = accept(mySocket, (struct sockaddr *) &address, &addrlen)) < 0) {
-		// 	if (errno == EAGAIN) { // no data available
-		// 	} else {
-		// 		perror("webserver accept");
-		// 		printf("%d ",new_socket);
-		// 		exit(1);
-		// 	}
-		// } else { // data available
-		// 	if (new_socket <= 0){
-		// 		perror("webserver connect:");
-		// 	}
-		// 	/* receive headder */
-		// 	// memset(bufferHTTP, 0, bufsize);
-		// 	fil_n(bufferHTTP, 0, bufsize);
-		// 	recv(new_socket, bufferHTTP, bufsize, 0);
-		// 	// printf("%s*ENDE*", bufferHTTP);
-		// 	getPostData(bufferHTTP,bufsize);
-		// 	// printBuffer(bufferHTTP,bufsize);
-		// 	/* send response */
-		// 	write(new_socket, "HTTP/1.1 200 OK\n", 16);
-		// 	write(new_socket, "Content-length: 7\n", 18);
-		// 	write(new_socket, "Content-Type: text/html\n\n", 25);
-		// 	write(new_socket, "200 OK\n",7);
-		// 	/* TODO: in PHP post-data will be send only after
-		// 	receiving the 200-OK-Header. Add or use the second buffer only!
-		// 	*/
-		// 	// /* receive posted data */
-		// 	// memset(bufferHTTP, 0, bufsize);
-		// 	// recv(new_socket, bufferHTTP, bufsize, 0);
-		// 	// printf("%s*ENDE*\n", bufferHTTP);
-		// 	// printBuffer(bufferHTTP,bufsize);
-		// 	// fflush(stdout);
-		// 	/* close this socket */
-		// 	close(new_socket);
-		// 	smiTxBuffer[0]= 0x50 | (smiId & 0x0f);
-		// 	smiTxBuffer[1]= smiCmd & 0x0f;
-		// 	smiTxSize=2;
-		// 	printBuffer(smiTxBuffer,smiTxSize);
-		// 	smiTxSize+=addSmiCrc(smiTxBuffer,smiTxSize);
-		// 	printBuffer(smiTxBuffer,smiTxSize);
-		// 	write(fdSmi,&smiTxBuffer,smiTxSize);
-		// 	printf("\n SMI command sent\n");
-		// }
+
+
+		/* web server */
+		tmpListen=listen(mySocket, 10);
+		if (tmpListen < 0) {
+			perror("webserver listen");
+			exit(1);
+		}
+
+		setNonblocking(mySocket);
+		if ((new_socket = accept(mySocket, (struct sockaddr *) &address, &addrlen)) < 0) {
+			if (errno == EAGAIN) { // no data available
+			} else {
+				perror("webserver accept");
+				printf("%d ",new_socket);
+				exit(1);
+			}
+		} else { // data available
+			if (new_socket <= 0){
+				perror("webserver connect:");
+			}
+			/* receive headder */
+			// memset(bufferHTTP, 0, bufsize);
+			// fill_n(bufferHTTP, 0, bufsize);
+			recv(new_socket, bufferHTTP, bufsize, 0);
+			// printf("%s*ENDE*", bufferHTTP);
+			getPostData(bufferHTTP,bufsize);
+			// printBuffer(bufferHTTP,bufsize);
+			/* send response */
+			write(new_socket, "HTTP/1.1 200 OK\n", 16);
+			write(new_socket, "Content-length: 7\n", 18);
+			write(new_socket, "Content-Type: text/html\n\n", 25);
+			write(new_socket, "200 OK\n",7);
+			/* TODO: in PHP post-data will be send only after
+			receiving the 200-OK-Header. Add or use the second buffer only!
+			*/
+			// /* receive posted data */
+			// memset(bufferHTTP, 0, bufsize);
+			// recv(new_socket, bufferHTTP, bufsize, 0);
+			// printf("%s*ENDE*\n", bufferHTTP);
+			// printBuffer(bufferHTTP,bufsize);
+			// fflush(stdout);
+			/* close this socket */
+			close(new_socket);
+			smiTxBuffer[0]= 0x50 | (smiId & 0x0f);
+			smiTxBuffer[1]= smiCmd & 0x0f;
+			smiTxSize=2;
+			printBuffer(smiTxBuffer,smiTxSize);
+			smiTxSize+=addSmiCrc(smiTxBuffer,smiTxSize);
+			printBuffer(smiTxBuffer,smiTxSize);
+			write(fdSmi,&smiTxBuffer,smiTxSize);
+			printf("\n SMI command sent\n");
+		}
 
 
 		/* SWB-Bus */
