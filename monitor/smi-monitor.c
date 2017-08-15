fb6461cb1a83e79b6b807a630f39b20a59c18424
ingof <ingo.fischer@arcormail.de>
2017-08-15 14:47:14 +0200

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -138,9 +138,9 @@ int main( int argc, char* argv[] ) {
 
 	tmpBind=bind(mySocket, (struct sockaddr *) &address, sizeof(address)) ;
 	if (tmpBind== 0) {
+		printf("addrlen; %d", addrlen);
+		printf("address; %d", address);
 		printf("Binding Socket %d\n",tmpBind);
-		printf("addrlen: %d", addrlen);
-		printf("address: %d", address);
 	} else perror("webserver bind");
 
 	perror(".");
