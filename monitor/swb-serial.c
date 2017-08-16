0d2878536d0a5ea88d809b37fcc34e0ba4a2def6
ingof <ingo.fischer@arcormail.de>
2017-08-16 21:01:59 +0200

    .

--- monitor/swb-serial.c
+++ monitor/swb-serial.c
@@ -1,16 +1,16 @@
 #include <stdio.h>			/* Standard input/output definitions */
 // #include <string.h>			/* String function definitions */
 // #include <unistd.h>			/* UNIX standard function definitions */
-// #include <fcntl.h>			/* File control definitions */
+#include <fcntl.h>			/* File control definitions */
 // #include <errno.h>			/* Error number definitions */
-// #include <termios.h>		/* POSIX terminal control definitions */
+#include <termios.h>		/* POSIX terminal control definitions */
 // #include <stdlib.h>			/* converting functions */
-#include <sys/ioctl.h>		/* ioctl() */
+// #include <sys/ioctl.h>		/* ioctl() */
 // #include "smi-monitor.h"	/* own funcions */
 // #include <sys/types.h>		/* ?? */
 // #include <sys/stat.h>		/* ?? */
 // #include <sys/time.h>		/* ?? */
-#include <linux/serial.h>	/* custom divisor */
+// #include <linux/serial.h>	/* custom divisor */
 
 #include "types.h"				/* type definitions */
 #include "swb-serial.h"			/* own functions */
