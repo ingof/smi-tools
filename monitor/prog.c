bacda3490063673c96df397edb956dabc7e8be0e
ingof <ingo.fischer@arcormail.de>
2017-08-17 17:21:10 +0200

    .

--- monitor/prog.c
+++ monitor/prog.c
@@ -1,26 +1,5 @@
 #include <stdio.h>			/* Standard input/output definitions */
-#include <string.h>			/* String function definitions */
-// #include <unistd.h>			/* UNIX standard function definitions */
-// #include <fcntl.h>			/* File control definitions */
-// #include <errno.h>			/* Error number definitions */
-// #include <termios.h>		/* POSIX terminal control definitions */
-// #include <stdlib.h>			/* converting functions */
-// #include <sys/ioctl.h>		/* ioctl() */
-// #include <sys/types.h>		/* ?? */
-// #include <sys/stat.h>		/* ?? */
-// #include <sys/time.h>		/* ?? */
-
-// #include "types.h"			/* type definitions */
-//
-//
-// #include<netinet/in.h>		/* web server */
-// #include<sys/socket.h>		/* web server */
-// #include<sys/stat.h>		/* web server */
-// #include<sys/types.h>		/* web server */
-// #include<unistd.h>			/* web server */
-
-
-
+#include <str.ing.h>			/* String function definitions */
 
 int main( int argc, char* argv[] ) {
 	printf(".");
@@ -30,5 +9,5 @@ int main( int argc, char* argv[] ) {
 	printf(".");
 	printf("%s\n",strchr(string, (int)'T'));
 	printf(".");
-	return 0;
+	return (0);
 }
