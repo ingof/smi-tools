b88f5c5b77a98059ef6d8febf9705aa31d1f2f69
ingof <ingo.fischer@arcormail.de>
2017-08-17 21:57:00 +0200

    .

--- monitor/smi-monitor.h
+++ monitor/smi-monitor.h
@@ -4,14 +4,14 @@
 
 
 /* print buffer to stdio (HEX)*/
-// void printBuffer(unsigned char *buffer, int size);
-void printBuffer(unsigned char *buffer, int size, int count);
+void printBuffer(unsigned char *buffer, int size);
 /* print buffer to stdio (ASCII)*/
 void printBufferAscii(unsigned char *buffer, int size);
 /* nonblocking for network socket */
 int setNonblocking(int fd);
 /* extract post data */
-int getPostData(unsigned char *buffer, int size);
+// int getPostData(unsigned char *buffer, int size);
+int getPostData(unsigned char *buffer, int size, int count);
 
 
 #endif /* smi-monitor.h */
