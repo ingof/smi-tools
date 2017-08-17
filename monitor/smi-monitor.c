391f3c1856e0ffc1ce4f8169e795b36edaa125b2
ingof <ingo.fischer@arcormail.de>
2017-08-17 20:47:05 +0200

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -217,7 +217,7 @@ int main( int argc, char* argv[] ) {
 				// fflush(stdout);
 				/* close this socket */
 				close(new_socket);
-				smiTxBuffer[0]= 0x50 | (smiId & 0x0f);
+				smiTxBuffer[0]= 0x40 | (smiId & 0x0f);
 				smiTxBuffer[1]= smiCmd & 0x0f;
 				smiTxSize=2;
 				// printBuffer(smiTxBuffer,smiTxSize);
