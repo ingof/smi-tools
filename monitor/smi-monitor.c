02c74b7a62ce5beafcea30102b2408b59a52eeba
ingof <ingo.fischer@arcormail.de>
2017-08-21 11:20:39 +0200

    .

--- monitor/smi-monitor.c
+++ monitor/smi-monitor.c
@@ -245,6 +245,47 @@ int main( int argc, char* argv[] ) {
 				// printBuffer(smiTxBuffer,smiTxSize);
 				write(fdSwb,&swbTxBuffer,swbTxSize);
 				// printf("\n SMI command sent\n");
+
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+				usleep(500);
+
+				swbTxBuffer[0]= 0xF0;
+				swbTxBuffer[1]= 0x24;
+				swbTxBuffer[2]= 0x6B;
+				swbTxBuffer[3]= 0x81;
+				swbTxBuffer[4]= 0x14;
+				swbTxBuffer[5]= 0xf8;	//crc
+				swbTxBuffer[6]= 0xce;	//crc
+				swbTxSize=7;
+				// printBuffer(smiTxBuffer,smiTxSize);
+				// swbTxSize+=addSwbCrc(smiTxBuffer,smiTxSize);
+				// printBuffer(smiTxBuffer,smiTxSize);
+				write(fdSwb,&swbTxBuffer,swbTxSize);
+				// printf("\n SMI command sent\n");
+
+
 			}
 		} /* Web-Server */
 
