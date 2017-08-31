2f647d3a919b730731271ba58a3cb81c2550cfdd
Ingo F <ems-gateway@arcor.de>
2017-08-31 19:57:07 +0200

    Update prog.c

--- monitor/prog.c
+++ monitor/prog.c
@@ -1,9 +1,11 @@
 #include <stdio.h>			/* Standard input/output definitions */
+#include <syslog.h>			/* Syslog */
 #include <string.h>			/* String function definitions */
 
 int main( int argc, char* argv[] ) {
 	char string[] = {"Ein Teststring mit Worten"};
 	printf("%s\n",strchr(string, (int)'W'));
 	printf("%s\n",strchr(string, (int)'T'));
+	syslog(LOG_SYSLOG, "prog: bendet");
 	return (0);
 }
